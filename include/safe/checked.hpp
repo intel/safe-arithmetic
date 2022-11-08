afe00b3741985bbd5d7ceaf7dbc26adf01c71c97
Luke Valenty <lvalenty@gmail.com>
2022-11-08 09:05:46 -0800

    add more checked operations

--- include/safe/checked.hpp
+++ include/safe/checked.hpp
@@ -4,6 +4,23 @@
 #include <type_traits>
 
 namespace safe {
+    namespace detail {
+        /*
+         * Compute log2 of integral types.
+         */
+        template<typename T>
+        [[nodiscard]] constexpr int log2(T value) {
+            int result = 0;
+
+            while (value != static_cast<T>(0)) {
+                value = value / static_cast<T>(2);
+                result++;
+            }
+
+            return result;
+        }
+    }
+
     template<typename T>
     struct checked {
     private:
@@ -34,30 +51,266 @@ namespace safe {
 
         template<typename U>
         [[nodiscard]] constexpr checked<bool> operator==(checked<U> rhs) const {
-            return {value_ == rhs.value_ && std::is_same_v<T, U>, overflow_ || rhs.overflow_};
+            return {value_ == rhs.value_, overflow_ || rhs.overflow_};
         }
 
+        // https://stackoverflow.com/questions/199333/how-do-i-detect-unsigned-integer-overflow
         template<typename U>
         [[nodiscard]] constexpr auto operator+(checked<U> rhs) const {
             using ret_t =
                 decltype(value_ + rhs.value_);
 
+            auto const left_value = static_cast<ret_t>(value_);
+            auto const right_value = static_cast<ret_t>(rhs.value_);
+
+            constexpr auto zero = static_cast<ret_t>(0);
+            constexpr auto max = std::numeric_limits<ret_t>::max();
+            constexpr auto lowest = std::numeric_limits<ret_t>::lowest();
+
             bool const overflow =
-                rhs.value_ > 0 && value_ > std::numeric_limits<ret_t>::max() - rhs.value_;
+                (right_value > zero) &
+                (left_value > max - right_value);
 
             bool const underflow = [&](){
                 if constexpr (std::numeric_limits<ret_t>::is_signed) {
-                    return rhs.value_ < 0 && value_ < std::numeric_limits<ret_t>::lowest() - rhs.value_;
+                    return
+                        (right_value < zero) &
+                        (left_value < lowest - right_value);
 
                 } else {
                     return false;
                 }
             }();
 
-            if (overflow || underflow || overflow_ || rhs.overflow_) {
-                return checked<ret_t>{0, true};
+            if (overflow | underflow | overflow_ | rhs.overflow_) {
+                return checked<ret_t>{zero, true};
             } else {
                 return checked<ret_t>{value_ + rhs.value_, false};
+            }
+        }
+
+        // https://stackoverflow.com/questions/199333/how-do-i-detect-unsigned-integer-overflow
+        template<typename U>
+        [[nodiscard]] constexpr auto operator-(checked<U> rhs) const {
+            using ret_t =
+                decltype(value_ - rhs.value_);
+
+            auto const left_value = static_cast<ret_t>(value_);
+            auto const right_value = static_cast<ret_t>(rhs.value_);
+
+            constexpr auto zero = static_cast<ret_t>(0);
+            constexpr auto max = std::numeric_limits<ret_t>::max();
+            constexpr auto lowest = std::numeric_limits<ret_t>::lowest();
+
+            bool const overflow =
+                (right_value < zero) &
+                (left_value > max + right_value);
+
+            bool const underflow =
+                (right_value > zero) &
+                (left_value < lowest + right_value);
+
+            if (overflow | underflow | overflow_ | rhs.overflow_) {
+                return checked<ret_t>{zero, true};
+            } else {
+                return checked<ret_t>{value_ - rhs.value_, false};
+            }
+        }
+
+        // https://stackoverflow.com/questions/199333/how-do-i-detect-unsigned-integer-overflow
+        template<typename U>
+        [[nodiscard]] constexpr auto operator*(checked<U> rhs) const {
+            using ret_t =
+                decltype(value_ * rhs.value_);
+
+            auto const left_value = static_cast<ret_t>(value_);
+            auto const right_value = static_cast<ret_t>(rhs.value_);
+
+            constexpr auto zero = static_cast<ret_t>(0);
+            constexpr auto max = std::numeric_limits<ret_t>::max();
+            constexpr auto lowest = std::numeric_limits<ret_t>::lowest();
+
+            bool const overflow =
+                (right_value != zero) &
+                (left_value > (max / right_value));
+
+            bool const underflow = [&](){
+                if constexpr (std::numeric_limits<ret_t>::is_signed) {
+                    return
+                        (right_value != zero) &
+                        (left_value < (lowest / right_value));
+
+                } else {
+                    return false;
+                }
+            }();
+
+            bool const negation_overflow = [&](){
+                if constexpr (
+                    std::numeric_limits<ret_t>::is_signed &&
+                    std::is_integral_v<ret_t>
+                ) {
+                    constexpr auto neg_one = static_cast<ret_t>(-1);
+
+                    return
+                        ((left_value == neg_one) & (right_value == lowest)) |
+                        ((right_value == neg_one) & (left_value == lowest));
+
+                } else {
+                    return false;
+                }
+            }();
+
+            if (overflow | underflow | negation_overflow | overflow_ | rhs.overflow_) {
+                return checked<ret_t>{zero, true};
+            } else {
+                return checked<ret_t>{value_ * rhs.value_, false};
+            }
+        }
+
+        // https://stackoverflow.com/questions/199333/how-do-i-detect-unsigned-integer-overflow
+        template<typename U>
+        [[nodiscard]] constexpr auto operator/(checked<U> rhs) const {
+            using ret_t =
+                decltype(value_ / rhs.value_);
+
+            auto const left_value = static_cast<ret_t>(value_);
+            auto const right_value = static_cast<ret_t>(rhs.value_);
+
+            constexpr auto zero = static_cast<ret_t>(0);
+            constexpr auto lowest = std::numeric_limits<ret_t>::lowest();
+
+            bool const divide_by_zero =
+                right_value == zero;
+
+            bool const negation_overflow = [&](){
+                if constexpr (
+                    std::numeric_limits<ret_t>::is_signed &&
+                    std::is_integral_v<ret_t>
+                ) {
+                    constexpr auto neg_one = static_cast<ret_t>(-1);
+
+                    return
+                        ((left_value == neg_one) & (right_value == lowest)) |
+                        ((right_value == neg_one) & (left_value == lowest));
+
+                } else {
+                    return false;
+                }
+            }();
+
+            if (divide_by_zero | negation_overflow | overflow_ | rhs.overflow_) {
+                return checked<ret_t>{zero, true};
+            } else {
+                return checked<ret_t>{value_ / rhs.value_, false};
+            }
+        }
+
+        template<typename U>
+        [[nodiscard]] constexpr auto operator%(checked<U> rhs) const {
+            using ret_t =
+                decltype(value_ % rhs.value_);
+
+            auto const left_value = static_cast<ret_t>(value_);
+            auto const right_value = static_cast<ret_t>(rhs.value_);
+
+            constexpr auto zero = static_cast<ret_t>(0);
+
+            bool const divide_by_zero =
+                right_value == zero;
+
+            if (divide_by_zero | overflow_ | rhs.overflow_) {
+                return checked<ret_t>{zero, true};
+            } else {
+                return checked<ret_t>{left_value % right_value, false};
+            }
+        }
+
+
+        template<typename U>
+        [[nodiscard]] constexpr auto operator<<(checked<U> rhs) const {
+            using ret_t = T;
+
+            int const ret_bit_width = std::numeric_limits<ret_t>::digits;
+            int const lhs_bit_width = detail::log2(value_);
+            int const shift_amt = rhs.value_;
+
+            bool const overflow =
+                (lhs_bit_width + shift_amt) > ret_bit_width;
+
+            bool const negative_shift =
+                shift_amt < 0;
+
+            if (overflow | negative_shift | overflow_ | rhs.overflow_) {
+                constexpr auto zero = static_cast<ret_t>(0);
+                return checked<ret_t>{zero, true};
+            } else {
+                return checked<ret_t>{value_ << shift_amt, false};
+            }
+        }
+
+        template<typename U>
+        [[nodiscard]] constexpr auto operator>>(checked<U> rhs) const {
+            using ret_t = T;
+
+            int const shift_amt = rhs.value_;
+
+            bool const negative_shift =
+                shift_amt < 0;
+
+            if (negative_shift | overflow_ | rhs.overflow_) {
+                constexpr auto zero = static_cast<ret_t>(0);
+                return checked<ret_t>{zero, true};
+            } else {
+                return checked<ret_t>{value_ >> shift_amt, false};
+            }
+        }
+
+        template<typename U>
+        [[nodiscard]] constexpr auto operator&(checked<U> rhs) const {
+            using ret_t =
+                decltype(value_ & rhs.value_);
+
+            auto const left_value = static_cast<ret_t>(value_);
+            auto const right_value = static_cast<ret_t>(rhs.value_);
+
+            if (overflow_ | rhs.overflow_) {
+                constexpr auto zero = static_cast<ret_t>(0);
+                return checked<ret_t>{zero, true};
+            } else {
+                return checked<ret_t>{left_value & right_value, false};
+            }
+        }
+
+        template<typename U>
+        [[nodiscard]] constexpr auto operator|(checked<U> rhs) const {
+            using ret_t =
+                decltype(value_ | rhs.value_);
+
+            auto const left_value = static_cast<ret_t>(value_);
+            auto const right_value = static_cast<ret_t>(rhs.value_);
+
+            if (overflow_ | rhs.overflow_) {
+                constexpr auto zero = static_cast<ret_t>(0);
+                return checked<ret_t>{zero, true};
+            } else {
+                return checked<ret_t>{left_value | right_value, false};
+            }
+        }
+
+        template<typename U>
+        [[nodiscard]] constexpr auto operator^(checked<U> rhs) const {
+            using ret_t =
+                decltype(value_ ^ rhs.value_);
+
+            auto const left_value = static_cast<ret_t>(value_);
+            auto const right_value = static_cast<ret_t>(rhs.value_);
+
+            if (overflow_ | rhs.overflow_) {
+                constexpr auto zero = static_cast<ret_t>(0);
+                return checked<ret_t>{zero, true};
+            } else {
+                return checked<ret_t>{left_value ^ right_value, false};
             }
         }
     };
--- test/safe/checked.cpp
+++ test/safe/checked.cpp
@@ -1,15 +1,188 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
 #include <safe.hpp>
 
-using namespace safe;
+
+using ::testing::_;
+using ::testing::Return;
+using ::testing::InSequence;
+
+
+using namespace safe::interval_types;
+using namespace safe::int_types;
 using namespace safe::literals;
 
-static_assert((checked{5} + checked{10} == checked{15}).value());
+using safe::checked;
 
-static_assert((checked{1500000000} + checked{1500000000}).is_overflow());
-static_assert((checked{1500000000} + checked{1500000000u} == checked{3000000000u}).value());
-static_assert((checked{-1} + checked{0u} == checked{-1u}).value());
-static_assert((checked{1} + checked{0u} == checked{1u}).value());
 
-int main() {
-    return 0;
+TEST(safe_checked_test, normal_addition) {
+    EXPECT_TRUE((checked{5} + checked{10} == checked{15}).value());
+    EXPECT_TRUE((checked{1500000000} + checked{1500000000u} == checked{3000000000u}).value());
+
+    EXPECT_TRUE((checked{-1} + checked{0} == checked{-1}).value());
+    EXPECT_TRUE((checked{1} + checked{0} == checked{1}).value());
+}
+
+TEST(safe_checked_test, overflow_addition) {
+    EXPECT_TRUE((checked{1500000000} + checked{1500000000}).is_overflow());
+}
+
+TEST(safe_checked_test, underflow_addition) {
+    EXPECT_TRUE((checked{-1500000000} + checked{-1500000000}).is_overflow());
+}
+
+TEST(safe_checked_test, overflow_plus_overflow) {
+    checked const a{0, true};
+    checked const b{0, true};
+    checked const c{0, false};
+
+    EXPECT_TRUE((a + b).is_overflow());
+    EXPECT_TRUE((a + c).is_overflow());
+    EXPECT_TRUE((c + b).is_overflow());
+    EXPECT_FALSE((c + c).is_overflow());
+}
+
+
+TEST(safe_checked_test, addition_edges) {
+    EXPECT_TRUE((checked{0} + checked{INT_MAX} == checked{INT_MAX}).value());
+    EXPECT_TRUE((checked{1} + checked{INT_MAX}).is_overflow());
+
+    EXPECT_TRUE((checked{0} + checked{INT_MIN} == checked{INT_MIN}).value());
+    EXPECT_TRUE((checked{-1} + checked{INT_MIN}).is_overflow());
+
+
+}
+
+TEST(safe_checked_test, normal_subtraction) {
+    EXPECT_TRUE((checked{5} - checked{10} == checked{-5}).value());
+    EXPECT_TRUE((checked{1500000000u} - checked{1500000000u} == checked{0u}).value());
+
+    EXPECT_TRUE((checked{-1} - checked{0} == checked{-1}).value());
+    EXPECT_TRUE((checked{1} - checked{0} == checked{1}).value());
+}
+
+TEST(safe_checked_test, overflow_subtraction) {
+    EXPECT_TRUE((checked{1500000000} - checked{-1500000000}).is_overflow());
+}
+
+TEST(safe_checked_test, underflow_subtraction) {
+    EXPECT_TRUE((checked{-1500000000} - checked{1500000000}).is_overflow());
+}
+
+TEST(safe_checked_test, overflow_minus_overflow) {
+    checked const a{0, true};
+    checked const b{0, true};
+    checked const c{0, false};
+
+    EXPECT_TRUE((a - b).is_overflow());
+    EXPECT_TRUE((a - c).is_overflow());
+    EXPECT_TRUE((c - b).is_overflow());
+    EXPECT_FALSE((c - c).is_overflow());
+}
+
+
+
+TEST(safe_checked_test, normal_multiplication) {
+    EXPECT_TRUE((checked{5} * checked{10} == checked{50}).value());
+    EXPECT_TRUE((checked{-1} * checked{0} == checked{0}).value());
+    EXPECT_TRUE((checked{1} * checked{18} == checked{18}).value());
+}
+
+TEST(safe_checked_test, overflow_multiplication) {
+    EXPECT_TRUE((checked{1500000000} * checked{1500000000}).is_overflow());
+
+    EXPECT_TRUE((checked{-1} * checked{std::numeric_limits<int>::lowest()}).is_overflow());
+    EXPECT_TRUE((checked{std::numeric_limits<int>::lowest()} * checked{-1}).is_overflow());
+}
+
+TEST(safe_checked_test, underflow_multiplication) {
+    EXPECT_TRUE((checked{-1500000000} * checked{1500000000}).is_overflow());
+}
+
+TEST(safe_checked_test, overflow_times_overflow) {
+    checked const a{0, true};
+    checked const b{0, true};
+    checked const c{0, false};
+
+    EXPECT_TRUE((a * b).is_overflow());
+    EXPECT_TRUE((a * c).is_overflow());
+    EXPECT_TRUE((c * b).is_overflow());
+    EXPECT_FALSE((c * c).is_overflow());
+}
+
+TEST(safe_checked_test, normal_division) {
+    EXPECT_TRUE((checked{12} / checked{6} == checked{2}).value());
+    EXPECT_TRUE((checked{-1000} / checked{10} == checked{-100}).value());
+    EXPECT_TRUE((checked{84} / checked{2} == checked{42}).value());
+}
+
+TEST(safe_checked_test, overflow_division) {
+    EXPECT_TRUE((checked{-1} / checked{std::numeric_limits<int>::lowest()}).is_overflow());
+    EXPECT_TRUE((checked{std::numeric_limits<int>::lowest()} / checked{-1}).is_overflow());
+}
+
+TEST(safe_checked_test, divide_by_zero) {
+    checked const a{1, false};
+    checked const b{0, false};
+
+    EXPECT_TRUE((a / b).is_overflow());
+}
+
+TEST(safe_checked_test, overflow_divided_by_overflow) {
+    checked const a{1, true};
+    checked const b{1, true};
+    checked const c{1, false};
+
+    EXPECT_TRUE((a / b).is_overflow());
+    EXPECT_TRUE((a / c).is_overflow());
+    EXPECT_TRUE((c / b).is_overflow());
+    EXPECT_FALSE((c / c).is_overflow());
+}
+
+TEST(safe_checked_test, normal_shift_left) {
+    EXPECT_TRUE(((checked{1u} << checked{31}) == checked{0x80000000u}).value());
+    EXPECT_TRUE(((checked{0u} << checked{32}) == checked{0u}).value());
+    EXPECT_TRUE(((checked{1u} << checked{15}) == checked{0x8000u}).value());
+    EXPECT_TRUE(((checked{1} << checked{15}) == checked{0x8000}).value());
+}
+
+TEST(safe_checked_test, overflow_shift_left) {
+    EXPECT_TRUE((checked{1} << checked{31}).is_overflow());
+    EXPECT_TRUE((checked{1u} << checked{32}).is_overflow());
+    EXPECT_TRUE((checked{0x80u} << checked{25}).is_overflow());
+    EXPECT_TRUE((checked{-256} << checked{23}).is_overflow());
+}
+
+TEST(safe_checked_test, negative_shift_left) {
+    EXPECT_TRUE((checked{256u} << checked{-1}).is_overflow());
+}
+
+TEST(safe_checked_test, overflow_shift_left_overflow) {
+    checked const a{1, true};
+    checked const b{1, true};
+    checked const c{1, false};
+
+    EXPECT_TRUE((a << b).is_overflow());
+    EXPECT_TRUE((a << c).is_overflow());
+    EXPECT_TRUE((c << b).is_overflow());
+    EXPECT_FALSE((c << c).is_overflow());
+}
+
+TEST(safe_checked_test, normal_shift_right) {
+    EXPECT_TRUE(((checked{0x80000000u} >> checked{31}) == checked{1u}).value());
+    EXPECT_TRUE(((checked{-16} >> checked{1}) == checked{-8}).value());
+    EXPECT_TRUE(((checked{-2} >> checked{1}) == checked{-1}).value());
+    EXPECT_TRUE(((checked{-1} >> checked{1}) == checked{-1}).value());
+}
+
+TEST(safe_checked_test, overflow_shift_right_overflow) {
+    checked const a{1, true};
+    checked const b{1, true};
+    checked const c{1, false};
+
+    EXPECT_TRUE((a >> b).is_overflow());
+    EXPECT_TRUE((a >> c).is_overflow());
+    EXPECT_TRUE((c >> b).is_overflow());
+    EXPECT_FALSE((c >> c).is_overflow());
 }
