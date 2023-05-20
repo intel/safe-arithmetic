ae756769a53630de1b49c9f12bbcece602dea3c6
Luke Valenty <luke.valenty@intel.com>
2023-05-20 14:44:26 -0700

    removing junk again

--- include/safe/algorithm_LOCAL_19443.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include <safe/algorithm/accumulate.hpp>
-#include <safe/algorithm/irange.hpp>
-#include <safe/algorithm/for_each.hpp>
--- include/safe/algorithm_REMOTE_19443.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-
-#include <safe/algorithm/accumulate.hpp>
