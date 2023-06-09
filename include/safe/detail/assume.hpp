a38744da2877f6a51b17193c5647d604f14f4989
Luke Valenty <luke.valenty@intel.com>
2023-06-09 15:33:52 -0700

    use gcc 13's __attribute__((assume(expr))) when available

--- include/safe/detail/assume.hpp
+++ include/safe/detail/assume.hpp
@@ -5,9 +5,16 @@
 #if defined(__cpp_assume)
     #define SAFE_ASSUME(expr) [[assume(expr)]]
 #elif defined(__clang__)
+    // https://clang.llvm.org/docs/LanguageExtensions.html#builtin-assume
     #define SAFE_ASSUME(expr) __builtin_assume(expr)
 #elif defined(__GNUC__) && !defined(__ICC)
-    #define SAFE_ASSUME(expr) if (expr) {} else { __builtin_unreachable(); }
+    #if __GNUC__ >= 13
+        // https://gcc.gnu.org/onlinedocs/gcc-13.1.0/gcc/Statement-Attributes.html#index-assume-statement-attribute
+        #define SAFE_ASSUME(expr) __attribute__((assume(expr)))
+    #else
+        // this is very questionable whether it does anything at all
+        #define SAFE_ASSUME(expr) if (expr) {} else { __builtin_unreachable(); }
+    #endif
 #elif defined(_MSC_VER) || defined(__ICC)
     #define SAFE_ASSUME(expr) __assume(expr)
 #endif
