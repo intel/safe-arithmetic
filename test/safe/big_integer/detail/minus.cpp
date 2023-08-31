e8d9aa031b4fe4266a7e90b32fa006b283332bae
Ben Deane <ben@elbeno.com>
2023-08-31 17:38:34 -0600

    :bug: Fix UB in integer overflow
    Some tests cause integer overflow: they need to be guarded by `RC_PRE`.
    This fixes signed 64-bit negation, addition and subtraction.
    Multiplication still has this problem.

--- test/safe/big_integer/detail/minus.cpp
+++ test/safe/big_integer/detail/minus.cpp
@@ -1,27 +1,29 @@
+#include "properties.hpp"
+#include "storage_gen.hpp"
+
+#include <safe/big_integer/detail/plus.hpp>
+#include <safe/big_integer/detail/storage.hpp>
+
 #include <gtest/gtest.h>
 #include <rapidcheck/gtest.h>
 
-#include "storage_gen.hpp"
-#include "properties.hpp"
-
-#include <safe/big_integer/detail/storage.hpp>
-#include <safe/big_integer/detail/plus.hpp>
-
-
 namespace safe::_big_integer::detail {
-    RC_GTEST_PROP(minus, identity, (storage<128> a)) {
-        identity<128, storage<32>{}>(minus, a);
-    }
-
-    RC_GTEST_PROP(minus, self_is_domination, (storage<128> a)) {
-        storage<128> actual{}; minus(actual, a, a);
-        storage<128> zero{};
-        RC_ASSERT(actual == zero);
-    }
-
-    RC_GTEST_PROP(minus, is_minus, (int64_t a, int64_t b)) {
-        storage<64> actual{}; minus(actual, make_storage(a), make_storage(b));
-        storage<64> expected = make_storage(a - b);
-        RC_ASSERT(actual == expected);
-    }
+RC_GTEST_PROP(minus, identity, (storage<128> a)) {
+    identity<128, storage<32>{}>(minus, a);
 }
+
+RC_GTEST_PROP(minus, self_is_domination, (storage<128> a)) {
+    storage<128> actual{};
+    minus(actual, a, a);
+    storage<128> zero{};
+    RC_ASSERT(actual == zero);
+}
+
+RC_GTEST_PROP(minus, is_minus, (int64_t a, int64_t b)) {
+    RC_PRE(subtraction_will_not_overflow(a, b));
+    storage<64> actual{};
+    minus(actual, make_storage(a), make_storage(b));
+    storage<64> expected = make_storage(a - b);
+    RC_ASSERT(actual == expected);
+}
+} // namespace safe::_big_integer::detail
--- test/safe/big_integer/detail/properties.hpp
+++ test/safe/big_integer/detail/properties.hpp
@@ -1,65 +1,96 @@
 #pragma once
 
-
-#include <rapidcheck/gtest.h>
-
 #include <safe/big_integer/detail/storage.hpp>
 
+#include <gtest/gtest.h>
+#include <rapidcheck/gtest.h>
+
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <limits>
 
 namespace safe::_big_integer::detail {
-    template<std::size_t LhsNumBits, std::size_t RhsNumBits>
-    void is_commutative(auto op, storage<LhsNumBits> a, storage<RhsNumBits> b) {
-        using result_storage = storage<std::max(LhsNumBits, RhsNumBits)>;
-        result_storage r0{}; op(r0, a, b);
-        result_storage r1{}; op(r1, b, a);
-        RC_ASSERT(r0 == r1);
-    }
-
-    template<typename ResultType>
-    void is_associative(auto op, auto a, auto b, auto c) {
-        ResultType r0{}; op(r0, a, b);
-        ResultType r1{}; op(r1, r0, c);
-
-        ResultType r2{}; op(r2, b, c);
-        ResultType r3{}; op(r3, a, r2);
-        RC_ASSERT(r3 == r1);
-    }
-
-    template<std::size_t NumBits>
-    void is_idempotent(auto op, storage<NumBits> a) {
-        storage<NumBits> result{}; op(result, a, a);
-        RC_ASSERT(a == result);
-    }
-
-    template<std::size_t NumBits, auto id>
-    void identity(auto op, storage<NumBits> a) {
-        storage<NumBits> result{}; op(result, a, id);
-        RC_ASSERT(a == result);
-    }
-
-    template<std::size_t NumBits, auto val>
-    void domination(auto op, storage<NumBits> a) {
-        storage<NumBits> result{}; op(result, a, val);
-        RC_ASSERT(val == result);
-    }
-
-    template<std::size_t DstBits, std::size_t SrcBits>
-    void assigned_value_equals_original(storage<SrcBits> a) {
-        storage<DstBits> result{};
-        result = a;
-        RC_ASSERT(result == a);
-    }
-
-    template<std::size_t DstBits, std::size_t SrcBits>
-    void copied_value_equals_original(storage<SrcBits> a) {
-        storage<DstBits> result{a};
-        RC_ASSERT(result == a);
-    }
-
-    template<typename ResultType>
-    void lhs_can_be_result(auto op, auto a, auto b) {
-        ResultType result0{}; op(result0, a, b);
-        ResultType result1{a}; op(result1, result1, b);
-        RC_ASSERT(result0 == result1);
-    }
+template <std::size_t LhsNumBits, std::size_t RhsNumBits>
+void is_commutative(auto op, storage<LhsNumBits> a, storage<RhsNumBits> b) {
+    using result_storage = storage<std::max(LhsNumBits, RhsNumBits)>;
+    result_storage r0{};
+    op(r0, a, b);
+    result_storage r1{};
+    op(r1, b, a);
+    RC_ASSERT(r0 == r1);
 }
+
+template <typename ResultType>
+void is_associative(auto op, auto a, auto b, auto c) {
+    ResultType r0{};
+    op(r0, a, b);
+    ResultType r1{};
+    op(r1, r0, c);
+
+    ResultType r2{};
+    op(r2, b, c);
+    ResultType r3{};
+    op(r3, a, r2);
+    RC_ASSERT(r3 == r1);
+}
+
+template <std::size_t NumBits> void is_idempotent(auto op, storage<NumBits> a) {
+    storage<NumBits> result{};
+    op(result, a, a);
+    RC_ASSERT(a == result);
+}
+
+template <std::size_t NumBits, auto id>
+void identity(auto op, storage<NumBits> a) {
+    storage<NumBits> result{};
+    op(result, a, id);
+    RC_ASSERT(a == result);
+}
+
+template <std::size_t NumBits, auto val>
+void domination(auto op, storage<NumBits> a) {
+    storage<NumBits> result{};
+    op(result, a, val);
+    RC_ASSERT(val == result);
+}
+
+template <std::size_t DstBits, std::size_t SrcBits>
+void assigned_value_equals_original(storage<SrcBits> a) {
+    storage<DstBits> result{};
+    result = a;
+    RC_ASSERT(result == a);
+}
+
+template <std::size_t DstBits, std::size_t SrcBits>
+void copied_value_equals_original(storage<SrcBits> a) {
+    storage<DstBits> result{a};
+    RC_ASSERT(result == a);
+}
+
+template <typename ResultType> void lhs_can_be_result(auto op, auto a, auto b) {
+    ResultType result0{};
+    op(result0, a, b);
+    ResultType result1{a};
+    op(result1, result1, b);
+    RC_ASSERT(result0 == result1);
+}
+
+template <typename T> constexpr auto negation_will_not_overflow(T a) -> bool {
+    return a != std::numeric_limits<T>::min();
+}
+
+template <typename T>
+constexpr auto addition_will_not_overflow(T a, T b) -> bool {
+    return std::signbit(a) != std::signbit(b) or
+           (a >= 0 and a <= std::numeric_limits<T>::max() - b) or
+           (a < 0 and a >= std::numeric_limits<T>::min() - b);
+}
+
+template <typename T>
+constexpr auto subtraction_will_not_overflow(T a, T b) -> bool {
+    return std::signbit(a) == std::signbit(b) or
+           (a >= 0 and a <= std::numeric_limits<T>::max() + b) or
+           (a < 0 and a >= std::numeric_limits<T>::min() + b);
+}
+} // namespace safe::_big_integer::detail
